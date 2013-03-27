02da07302b6180259b30feea58053be58fbea80a
David Volm <david@volminator.com>
2013-03-26 20:51:37 -0500

    updated

--- camptrak-import.js
+++ camptrak-import.js
@@ -2,7 +2,7 @@ plugin('camptrak-import', function(backend, frontend) {
     backend('import', function(Canteen, emit, data) {
         Canteen.db.accounts();
         Canteen.log(data.import);
-        emit('/');
+        emit('./');
         //emit({"json": true});
     });
     
