b7700c38297c8ce032a47b4a349214d7a60efebe
David Volm <david@volminator.com>
2013-05-01 17:32:47 -0500

    Just percents.

--- staff-discounts.js
+++ staff-discounts.js
@@ -1,5 +1,5 @@
 /* staff-discounts
- * Allow discounts by amount or percent
+ * Allow discounts by percent
  * (c) 2013 David (daXXog) Volm ><> + + + <><
  * Released under Apache License, Version 2.0:
  * http://www.apache.org/licenses/LICENSE-2.0.html  
